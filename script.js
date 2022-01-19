242c85b2f04c9e86a1294b80936e822fbf786346
ivbq <ivar.boqvist@elev.ga.ntig.se>
2022-01-19 01:04:27 +0100

    some big-ass changes, mostly good

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-// TODO: Scroll in when clicking on school in list/school as node
+// Planned features:
+// TODO: Allow for sorting and filtering schools
+// TODO: Only list visible nodes
+// TODO: Mobile support
+// TODO: Add accessibility options?
 
+// Small issues:
 // TODO: Fix issue where multiple school windows can be open at the same time
-
-// TODO: Allow for filtering schools
-// Apply filter to list of schools, only render in items/nodes that fit
-
-// TODO: Show information about nearest station: name, street, distance
-
+// TODO: Fix coloring issue with polygons
+// TODO: Add rudimentary keyboard control
 
 // Initialisation
 let map = L.map('leaflet').setView([59.3, 18.075], 13);
@@ -33,38 +34,38 @@ fetch('geometry.geojson')
         nodeLayer.addData(nodes);
         return nodes;
     })
-    .then(nodes => {
-        nodes.forEach(node => {
-            constructSchool(node);
-        })
-    })
+    .then(nodes => nodes.forEach(node => constructSchool(node)))
 
 nodeLayer.on("click", (event) => {
-    let node = event.layer.feature;
-    console.log(node);
+    const node = event.layer.feature;
+    const id = node.properties.id;
 
-    getIsochrone(node);
-    getPathToNearest(node);
+    // Calls function associated with event handler on list object
+    // TODO: Refactor this previously mentioned event handler out
+    document.getElementById(`i${id}`).previousElementSibling.children[0].click();
 });
 
 map.on("movestart", () => {
-    console.log("Moooove?");
+    updateNodeList();
 })
 
 // Definitions
-function getIsochrone(node) {
+async function getIsochrone(node) {
     const index = nodes.indexOf(node);
     const isochrone = isochrones[index];
     isoLayer.clearLayers();
-    isoLayer.addData(isochrone);
+    L.vectorGrid.slicer(isochrone, isoOptions).addTo(isoLayer);
 }
 
-function getPathToNearest(node) {
+async function getPathToNearest(node) {
     const index = nodes.indexOf(node);
-    const direction = directions[index];
-    const path = direction.routes[0].geometry;
+    const path = directions[index].routes[0].geometry;
     dirLayer.clearLayers();
-    dirLayer.addData(path);
+    L.vectorGrid.slicer(path, dirOptions).addTo(dirLayer);
+}
+
+function updateNodeList() {
+    return;
 }
 
 // I blame this mess on Javascript's handling (or lack thereof) of references
@@ -82,12 +83,19 @@ function constructSchool(node) {
     school.children[1].children[0].children[0].innerText = node.properties.name;
     school.children[1].children[0].children[1].innerText = node.properties.form;
 
-    school.children[1].children[0].children[2].innerHTML = "";
+    // Information about nearest station
+    const indexNode = nodes.indexOf(node);
+    const direction = directions[indexNode];
+    const distance = direction.routes[0].distance;
+    school.children[1].children[0].children[2].innerText = `Avstånd till närmsta station: ${distance.toFixed(0)}m`;
+
+    // Populate body with available programs
+    school.children[1].children[0].children[3].innerHTML = "";
     for (const [key, value] of Object.entries(node.properties)) {
         if (value == "J") {
             let listElement = document.createElement("li");
             listElement.innerHTML = program[key];
-            school.children[1].children[0].children[2].appendChild(listElement);
+            school.children[1].children[0].children[3].appendChild(listElement);
         }
     }
 
@@ -95,14 +103,30 @@ function constructSchool(node) {
         const id = event.target.id.slice(1);
         Object.entries(nodeLayer._layers).forEach(([key, value]) => {
             if (value.feature.properties.id == id) {
-                map.setView(value._latlng);
                 getIsochrone(value.feature);
                 getPathToNearest(value.feature);
+                map.flyTo(value._latlng, 15);
             }
         })
     })
 
     document.querySelector("#accordion").appendChild(school);
+}
+
+const isoOptions = {
+    vectorTileLayerStyles: {
+        inner: {},
+        middle: {},
+        outer: {}
+    },
+    maxZoom: 20
+}
+
+const dirOptions = {
+    vectorTileLayerStyles: {
+        path: {}
+    },
+    maxZoom: 20
 }
 
 const program = {
